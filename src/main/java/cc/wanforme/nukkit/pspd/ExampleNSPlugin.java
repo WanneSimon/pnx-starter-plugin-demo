3164d9bf6b9a6096625eca125eff7325c48bd082
wanne <wanneseville@foxmail.com>
2022-08-05 22:39:13 +0800

    init

---
+++ src/main/java/cc/wanforme/nukkit/pspd/ExampleNSPlugin.java
@@ -0,0 +1,53 @@
+package cc.wanforme.nukkit.pspd;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import cc.wanforme.nukkit.springDemo.DashInfo;
+
+@Component
+public class ExampleNSPlugin extends PluginBase {
+
+	@Autowired
+	@Qualifier("dash1")
+	private DashInfo dash1;
+	
+	@Autowired
+	@Qualifier("dash2")
+	private DashInfo dash2;
+	
+    @Override
+    public void onLoad() {
+        this.getLogger().info(TextFormat.WHITE + "I've been loaded!");
+        getLogger().info("dash1: " + dash1.toString());
+    }
+
+    @Override
+    public void onEnable() {
+    	getLogger().info("dash2: " + dash2.toString());
+    	
+        this.getLogger().info(TextFormat.DARK_GREEN + "I've been enabled!");
+
+        this.getLogger().info(String.valueOf(this.getDataFolder().mkdirs()));
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info(TextFormat.DARK_RED + "I've been disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if ("test".equals(command.getName().toLowerCase())) {
+        	 this.getLogger().info("hello");
+        }
+        return true;
+    }
+
+}
